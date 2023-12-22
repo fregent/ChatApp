88c0eab424eda729e53dd510d84974b038660d6c
Frederic REGENT <frederic.regent97@gmail.com>
2023-12-22 21:10:57 +0100

    Gem jwt installed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
+
+  def generate_token
+    payload = { user_id: self.id }
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 end
